f5647c6fbf450d7a861cf918a62bdedb59bf4b58
Ethan Jones <339467243@GoTVDSB.ca>
2024-03-01 14:23:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace Topic_5___Descision_Structues_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Banking Machine
+            BankOfBlob();
+            
+        }
+
+        static void BankOfBlob()
+        {
+            bool activeBanks = true;
+            while (activeBanks == true)
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
